d99217fc104da23bfeb7060fd398e961ee6d8ec4
anh.vuduc <anh.vuduc@timego.biz>
2023-08-16 23:26:38 +0700

    Add 3D sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,216 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+import ThreeGlobe from "three-globe";
+import { WebGLRenderer, Scene, AxesHelper } from "three";
+import {
+  PerspectiveCamera,
+  AmbientLight,
+  DirectionalLight,
+  Color,
+  Fog,
+  // AxesHelper,
+  // DirectionalLightHelper,
+  // CameraHelper,
+  PointLight,
+  SphereGeometry,
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { createGlowMesh } from "three-glow-mesh";
+import countries from "./files/globe-data-min.json";
+import travelHistory from "./files/my-flights.json";
+import airportHistory from "./files/my-airports.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    init();
+    initGlobe();
+    onWindowResize();
+    animate();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id="global" style={{ width: "100%" }}></div>
     </>
-  )
+  );
 }
 
-export default App
+let renderer, camera, scene, controls;
+let mouseX = 0;
+let mouseY = 0;
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+let Globe;
+
+// SECTION Initializing core ThreeJS elements
+function init() {
+  // Initialize renderer
+  renderer = new WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // renderer.outputEncoding = THREE.sRGBEncoding;
+  document.getElementById("global")?.appendChild(renderer.domElement);
+
+  // Initialize scene, light
+  scene = new Scene();
+  scene.add(new AmbientLight(0x242742, 0.3));
+  scene.background = new Color(0x453469);
+
+  // Initialize camera, light
+  camera = new PerspectiveCamera();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  let dLight = new DirectionalLight(0xffffff, 0.8);
+  dLight.position.set(-800, 2000, 400);
+  camera.add(dLight);
+
+  let dLight1 = new DirectionalLight(0x7982f6, 1);
+  dLight1.position.set(-200, 500, 200);
+  camera.add(dLight1);
+
+  let dLight2 = new PointLight(0x8566cc, 0.5);
+  dLight2.position.set(-200, 500, 200);
+  camera.add(dLight2);
+
+  const axesHelper = new AxesHelper(500);
+  scene.add(axesHelper);
+  camera.position.z = 5;
+  camera.position.x = 0;
+  camera.position.y = 0;
+
+  scene.add(camera);
+
+  // Additional effects
+  scene.fog = new Fog(0x535ef3, 400, 2000);
+
+  // Helpers
+  // const axesHelper = new AxesHelper(800);
+  // scene.add(axesHelper);
+  // var helper = new DirectionalLightHelper(dLight);
+  // scene.add(helper);
+  // var helperCamera = new CameraHelper(dLight.shadow.camera);
+  // scene.add(helperCamera);
+
+  // Initialize controls
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dynamicDampingFactor = 0.01;
+  controls.enablePan = false;
+  controls.minDistance = 200;
+  controls.maxDistance = 500;
+  controls.rotateSpeed = 0.8;
+  controls.zoomSpeed = 1;
+  controls.autoRotate = false;
+
+  controls.minPolarAngle = Math.PI / 3.5;
+  controls.maxPolarAngle = Math.PI - Math.PI / 3;
+
+  window.addEventListener("resize", onWindowResize, false);
+  document.addEventListener("mousemove", onMouseMove);
+}
+
+// SECTION Globe
+function initGlobe() {
+  // Initialize the Globe
+  Globe = new ThreeGlobe({
+    waitForGlobeReady: true,
+    animateIn: true,
+  })
+    .hexPolygonsData(countries.features) // render node countruy
+    .hexPolygonResolution(3)
+    .hexPolygonMargin(0.7)
+    .showAtmosphere(false)
+    // .atmosphereColor("#3a228a")
+    .atmosphereAltitude(0.25)
+    .hexPolygonColor((e) => {
+      if (
+        ["KGZ", "KOR", "THA", "RUS", "UZB", "IDN", "KAZ", "MYS"].includes(
+          e.properties.ISO_A3,
+        )
+      ) {
+        return "rgb(131, 104, 155)";
+      } else return "rgb(131, 104, 155)";
+    });
+
+  // NOTE Arc animations are followed after the globe enters the scene
+  setTimeout(() => {
+    Globe.arcsData(travelHistory.flights)
+      .arcColor((e) => {
+        return "#09C4A2";
+        return e.status ? "#9cff00" : "#FF4000";
+      })
+      .arcAltitude((e) => {
+        return e.arcAlt;
+      })
+      .arcStroke((e) => {
+        return e.status ? 0.5 : 0.3;
+      })
+      .arcDashLength(0.9)
+      .arcDashGap(4)
+      .arcDashAnimateTime(1000)
+      .arcsTransitionDuration(1000)
+      .arcDashInitialGap((e) => e.order * 1)
+      .labelsData(airportHistory.airports)
+      .labelColor(() => "#ffcb21")
+      .labelDotOrientation((e) => {
+        return e.text === "ALA" ? "top" : "right";
+      })
+      .labelDotRadius(0.3)
+      .labelSize((e) => e.size)
+      .labelText("city")
+      .labelResolution(6)
+      .labelAltitude(0.01)
+      .pointsData(airportHistory.airports)
+      .pointColor(() => "#ffffff")
+      .pointsMerge(true)
+      .pointAltitude(0.07)
+      .pointRadius(0.05);
+  }, 1000);
+
+  Globe.rotateY(-Math.PI * (5 / 9));
+  Globe.rotateZ(-Math.PI / 6);
+  const globeMaterial = Globe.globeMaterial();
+  globeMaterial.color = new Color(0x3d2754);
+  globeMaterial.emissive = new Color(0x1b2338);
+  globeMaterial.emissiveIntensity = 0.1;
+  globeMaterial.shininess = 0.7;
+
+  // NOTE Cool stuff
+  // globeMaterial.wireframe = true;
+
+  scene.add(Globe);
+}
+
+function onMouseMove(event) {
+  mouseX = event.clientX - windowHalfX;
+  mouseY = event.clientY - windowHalfY;
+  // console.log("x: " + mouseX + " y: " + mouseY);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  windowHalfX = window.innerWidth / 1.5;
+  windowHalfY = window.innerHeight / 1.5;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  // camera.position.x +=
+  //   Math.abs(mouseX) <= windowHalfX / 2
+  //     ? (mouseX / 2 - camera.position.x) * 0.005
+  //     : 0;
+  // camera.position.y += (-mouseY / 2 - camera.position.y) * 0.005;
+  // camera.lookAt(scene.position);
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+export default App;
