6c6465f94f489f2705ca798b97fb00cc4536ef02
phunguyentan109 <phulicon815490@gmail.com>
2023-08-22 16:21:11 +0700

    Fix not showing glob on dev mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import ThreeGlobe from "three-globe";
-import { WebGLRenderer, Scene } from "three";
+import ThreeGlobe from 'three-globe'
+import { WebGLRenderer, Scene } from 'three'
 import {
   PerspectiveCamera,
   AmbientLight,
@@ -7,55 +7,57 @@ import {
   Color,
   Fog,
   PointLight,
-} from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import countries from "./assets/globe-data-min.json";
-import travelHistory from "./assets/my-flights.json";
-import airportHistory from "./assets/my-airports.json";
-import { useEffect } from "react";
-var renderer, camera, scene, controls;
-let mouseX = 0;
-let mouseY = 0;
-let windowHalfX = window.innerWidth / 2;
-let windowHalfY = window.innerHeight / 2;
-var Globe;
+} from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import countries from './assets/globe-data-min.json'
+import travelHistory from './assets/my-flights.json'
+import airportHistory from './assets/my-airports.json'
+import { useEffect } from 'react'
+
+var renderer, camera, scene, controls
+let mouseX = 0
+let mouseY = 0
+let windowHalfX = window.innerWidth / 2
+let windowHalfY = window.innerHeight / 2
+var Globe
 
 function init() {
   // Initialize renderer
-  renderer = new WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.getElementById("3d-glob").appendChild(renderer.domElement);
+  renderer = new WebGLRenderer({ antialias: true })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  document.getElementById('3d-glob').appendChild(renderer.domElement)
 
   // Initialize scene, light
-  scene = new Scene();
-  scene.add(new AmbientLight(0xbbbbbb, 0.3));
+  scene = new Scene()
+  scene.add(new AmbientLight(0xbbbbbb, 0.3))
 
   // Initialize camera, light
-  camera = new PerspectiveCamera();
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+  camera = new PerspectiveCamera()
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
 
-  var dLight = new DirectionalLight(0xffffff, 0.8);
-  dLight.position.set(-800, 2000, 400);
-  camera.add(dLight);
+  var dLight = new DirectionalLight(0xffffff, 0.8)
+  dLight.position.set(-800, 2000, 400)
+  camera.add(dLight)
 
-  var dLight1 = new DirectionalLight(0x7982f6, 1);
-  dLight1.position.set(-200, 500, 200);
-  camera.add(dLight1);
+  var dLight1 = new DirectionalLight(0x7982f6, 1)
+  dLight1.position.set(-200, 500, 200)
+  camera.add(dLight1)
 
-  var dLight2 = new PointLight(0x8566cc, 0.5);
-  dLight2.position.set(-200, 500, 200);
-  camera.add(dLight2);
+  var dLight2 = new PointLight(0x8566cc, 0.5)
+  dLight2.position.set(-200, 500, 200)
+  camera.add(dLight2)
 
-  camera.position.z = 400;
-  camera.position.x = 0;
-  camera.position.y = 0;
+  camera.position.z = 400
+  camera.position.x = 0
+  camera.position.y = 0
 
-  scene.add(camera);
+  scene.add(camera)
 
   // Additional effects
-  scene.fog = new Fog(0x535ef3, 400, 2000);
+  scene.fog = new Fog(0x535ef3, 400, 2000)
 
   // Helpers
   // const axesHelper = new AxesHelper(800);
@@ -66,21 +68,21 @@ function init() {
   // scene.add(helperCamera);
 
   // Initialize controls
-  controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.dynamicDampingFactor = 0.01;
-  controls.enablePan = false;
-  controls.minDistance = 200;
-  controls.maxDistance = 500;
-  controls.rotateSpeed = 0.8;
-  controls.zoomSpeed = 1;
-  controls.autoRotate = false;
+  controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableDamping = true
+  controls.dynamicDampingFactor = 0.01
+  controls.enablePan = false
+  controls.minDistance = 200
+  controls.maxDistance = 500
+  controls.rotateSpeed = 0.8
+  controls.zoomSpeed = 1
+  controls.autoRotate = false
 
-  controls.minPolarAngle = Math.PI / 3.5;
-  controls.maxPolarAngle = Math.PI - Math.PI / 3;
+  controls.minPolarAngle = Math.PI / 3.5
+  controls.maxPolarAngle = Math.PI - Math.PI / 3
 
-  window.addEventListener("resize", onWindowResize, false);
-  document.addEventListener("mousemove", onMouseMove);
+  window.addEventListener('resize', onWindowResize, false)
+  document.addEventListener('mousemove', onMouseMove)
 }
 
 // SECTION Globe
@@ -98,25 +100,25 @@ function initGlobe() {
     .atmosphereAltitude(0.25)
     .hexPolygonColor((e) => {
       if (
-        ["KGZ", "KOR", "THA", "RUS", "UZB", "IDN", "KAZ", "MYS"].includes(
-          e.properties.ISO_A3
+        ['KGZ', 'KOR', 'THA', 'RUS', 'UZB', 'IDN', 'KAZ', 'MYS'].includes(
+          e.properties.ISO_A3,
         )
       ) {
-        return "rgba(255,255,255, 1)";
-      } else return "rgba(255,255,255, 0.7)";
-    });
+        return 'rgba(255,255,255, 1)'
+      } else return 'rgba(255,255,255, 0.7)'
+    })
 
   // NOTE Arc animations are followed after the globe enters the scene
   setTimeout(() => {
     Globe.arcsData(travelHistory.flights)
       .arcColor((e) => {
-        return e.status ? "#09C4A2" : "#B3ABB8";
+        return e.status ? '#09C4A2' : '#B3ABB8'
       })
       .arcAltitude((e) => {
-        return e.arcAlt;
+        return e.arcAlt
       })
       .arcStroke((e) => {
-        return e.status ? 0.5 : 0.3;
+        return e.status ? 0.5 : 0.3
       })
       .arcDashLength(0.5)
       .arcDashGap(4)
@@ -124,62 +126,67 @@ function initGlobe() {
       .arcsTransitionDuration(1000)
       .arcDashInitialGap((e) => e.order * 1)
       .labelsData(airportHistory.airports)
-      .labelColor(() => "#ffcb21")
+      .labelColor(() => '#ffcb21')
       .labelSize((e) => e.size)
-      .labelText("city")
+      .labelText('city')
       .labelResolution(6)
       .labelAltitude(0.03)
       .labelIncludeDot(false)
-  }, 1000);
+  }, 1000)
 
+  const globeMaterial = Globe.globeMaterial()
+  globeMaterial.emissive = new Color(0x220038)
+  globeMaterial.emissiveIntensity = 0.1
+  globeMaterial.shininess = 0.7
+  globeMaterial.wireframe = true
 
-  const globeMaterial = Globe.globeMaterial();
-  globeMaterial.emissive = new Color(0x220038);
-  globeMaterial.emissiveIntensity = 0.1;
-  globeMaterial.shininess = 0.7;
-  globeMaterial.wireframe = true;
-
-  scene.add(Globe);
+  scene.add(Globe)
 }
 
 function onMouseMove(event) {
-  mouseX = event.clientX - windowHalfX;
-  mouseY = event.clientY - windowHalfY;
+  mouseX = event.clientX - windowHalfX
+  mouseY = event.clientY - windowHalfY
   // console.log("x: " + mouseX + " y: " + mouseY);
 }
 
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  windowHalfX = window.innerWidth / 1.5;
-  windowHalfY = window.innerHeight / 1.5;
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  windowHalfX = window.innerWidth / 1.5
+  windowHalfY = window.innerHeight / 1.5
+  renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
 function animate() {
   camera.position.x +=
     Math.abs(mouseX) <= windowHalfX / 2
       ? (mouseX / 2 - camera.position.x) * 0.005
-      : 0;
-  camera.position.y += (-mouseY / 2 - camera.position.y) * 0.005;
-  camera.lookAt(scene.position);
-  controls.update();
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+      : 0
+  camera.position.y += (-mouseY / 2 - camera.position.y) * 0.005
+  camera.lookAt(scene.position)
+  controls.update()
+  renderer.render(scene, camera)
+  requestAnimationFrame(animate)
 }
 
 function App() {
   useEffect(() => {
-    init();
-initGlobe();
-onWindowResize();
-animate();
-   }, [])
-  return (
-    <div id="3d-glob">
+    init()
+    initGlobe()
+    onWindowResize()
+    animate()
 
+    return () => {
+      const globPlace = document.getElementById('3d-glob')
+      if (globPlace) globPlace.innerHTML = ''
+    }
+  }, [])
+
+  return (
+    <div>
+      <div id='3d-glob'></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
