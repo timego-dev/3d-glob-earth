17a405c78d264de930a24f86ca0d2e230d179576
phunguyentan109 <phulicon815490@gmail.com>
2023-08-25 16:29:56 +0700

    Refactor code, done hide location point on filter, working on hiding location name on filter

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,24 +1,4 @@
-import ThreeGlobe from 'three-globe'
-import {
-  AmbientLight,
-  CircleGeometry,
-  Color,
-  DirectionalLight,
-  DoubleSide,
-  Fog,
-  Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
-  PointLight,
-  RingGeometry,
-  Scene,
-  WebGLRenderer,
-} from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import countries from '../../assets/globe-data-min.json'
-import travelHistory from '../../assets/my-flights.json'
-import airportHistory from '../../assets/my-airports.json'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { Button, chakra, HStack } from '@chakra-ui/react'
 import Panel from './components/Panel'
 import { DEFAULT_LOCATION } from './constants/const'
@@ -29,251 +9,122 @@ import {
   navButtonStyle,
 } from './constants/style'
 import RadioIcon from './components/Circle'
-import { CSS2DRenderer } from 'three/addons/renderers/CSS2DRenderer'
-
-var renderer, cssRenderer, camera, scene, controls
-// let mouseX = 0
-// let mouseY = 0
-// let windowHalfX = window.innerWidth / 2
-// let windowHalfY = window.innerHeight / 2
-var Globe
-
-function init() {
-  // Initialize renderer
-  renderer = new WebGLRenderer({ antialias: true })
-
-  renderer.setClearColor(0x000000, 0)
-
-  renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setSize(window.innerWidth, window.innerHeight)
-
-  document.getElementById('3d-glob').appendChild(renderer.domElement)
-
-  cssRenderer = new CSS2DRenderer()
-
-  cssRenderer.setSize(window.innerWidth, window.innerHeight)
-  cssRenderer.domElement.style.position = 'absolute'
-  cssRenderer.domElement.style.top = '-32px'
-  cssRenderer.domElement.style.pointerEvents = 'none'
-
-  document.getElementById('3d-glob').appendChild(cssRenderer.domElement)
-
-  // Initialize scene, light
-  scene = new Scene()
-  // scene.background = new Color('transparent')
-  scene.add(new AmbientLight(0xbbbbbb, 0.3))
-
-  // Initialize camera, light
-  camera = new PerspectiveCamera()
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-
-  var dLight = new DirectionalLight(0xffffff, 0.8)
-  dLight.position.set(-800, 2000, 400)
-  camera.add(dLight)
-
-  var dLight1 = new DirectionalLight(0x7982f6, 1)
-  dLight1.position.set(-200, 500, 200)
-  camera.add(dLight1)
-
-  var dLight2 = new PointLight(0x8566cc, 0.5)
-  dLight2.position.set(-200, 500, 200)
-  camera.add(dLight2)
-
-  camera.position.z = 400
-  camera.position.x = 20000
-  camera.position.y = 5000
-
-  camera.zoom = 1.7
-
-  scene.add(camera)
-
-  // Additional effects
-  scene.fog = new Fog(0x535ef3, 400, 2000)
-
-  // Helpers
-  // const axesHelper = new AxesHelper(800);
-  // scene.add(axesHelper);
-  // var helper = new DirectionalLightHelper(dLight);
-  // scene.add(helper);
-  // var helperCamera = new CameraHelper(dLight.shadow.camera);
-  // scene.add(helperCamera);
-
-  // Initialize controls
-  controls = new OrbitControls(camera, renderer.domElement)
-  controls.enableDamping = true
-  controls.dynamicDampingFactor = 0.01
-  controls.enablePan = false
-  controls.minDistance = 200
-  controls.maxDistance = 400
-  controls.rotateSpeed = 0.8
-  controls.zoomSpeed = 1
-  controls.autoRotate = false
-
-  controls.minPolarAngle = Math.PI / 3.5
-  controls.maxPolarAngle = Math.PI - Math.PI / 3
-
-  window.addEventListener('resize', onWindowResize, false)
-  // document.addEventListener('mousemove', onMouseMove)
-}
-
-const renderAirport = (name) => {
-  return `<div
-   style="color: #FAF6FE;
-   background: #ffffff29;
-   line-height: 27px;
-   font-family: Futura Round Medium;
-   padding: 0 24px;
-   border-radius: 40px;
-   font-size: 16px;
-   border: 1px solid rgba(159, 115, 202, 0.5)">
-   ${name}
-   </div>`
-}
-
-// SECTION Globe
-function initGlobe() {
-  // Initialize the Globe
-  Globe = new ThreeGlobe({
-    waitForGlobeReady: true,
-    animateIn: true,
-  })
-    .showGlobe(false)
-
-    .htmlElementsData(airportHistory.airports)
-    .htmlElement((d) => {
-      const el = document.createElement('div')
-      el.innerHTML = renderAirport(d.city)
-      return el
-    })
-
-    .hexPolygonsData(countries.features)
-    .hexPolygonResolution(3)
-    .hexPolygonMargin(0.7)
-    .showAtmosphere(false)
-    // .atmosphereColor("red")
-    .atmosphereAltitude(0.25)
-    .hexPolygonColor((e) => {
-      if (
-        ['KGZ', 'KOR', 'THA', 'RUS', 'UZB', 'IDN', 'KAZ', 'MYS'].includes(
-          e.properties.ISO_A3,
-        )
-      ) {
-        return 'rgba(255,255,255, 1)'
-      } else return 'rgba(255,255,255, 0.7)'
-    })
-
-    .customLayerData(airportHistory.airports)
-    .customThreeObject((d) => {
-      let circle = new Mesh(
-        new CircleGeometry(d.isInactive ? 0.4 : 0.6)
-          .translate(0, 0, 1.01)
-          .rotateX(Math.PI),
-        new MeshBasicMaterial({ color: '#00FECD', side: DoubleSide }),
-      )
-
-      const ringInner = d.isInactive ? 1.4 : 0.6
-      const ringOuter = 1.5
-
-      const ring = new Mesh(
-        new RingGeometry(ringInner, ringOuter)
-          .translate(0, 0, 1)
-          .rotateX(Math.PI),
-        new MeshBasicMaterial({
-          color: 'rgba(4, 164, 135, 0.5)',
-          side: DoubleSide,
-        }),
-      )
-
-      circle.add(ring)
-      ring.lookAt(0, 0, 0)
-
-      return circle
-    })
-    .customThreeObjectUpdate((obj, d) => {
-      Object.assign(obj.position, Globe.getCoords(d.lat, d.lng, d.alt))
-
-      obj.lookAt(0, 0, 0)
-    })
-
-  // NOTE Arc animations are followed after the globe enters the scene
-  setTimeout(() => {
-    Globe.arcsData(travelHistory.flights)
-      .arcColor((e) => {
-        return e.status ? 'rgba(9, 196, 162, 1)' : 'rgba(179, 171, 184, 1)'
-      })
-      .arcAltitude((e) => {
-        return e.arcAlt
-      })
-      .arcStroke((e) => {
-        return e.status ? 0.5 : 0.3
-      })
-      .arcDashLength(0.5)
-      .arcDashGap(4)
-      .arcDashAnimateTime(1000)
-      .arcsTransitionDuration(1000)
-      .arcDashInitialGap((e) => e.order * 1)
-
-    // .pointsData(airportHistory.airports)
-    // .pointColor(() => 'aqua')
-    // .pointAltitude(0.07)
-    // .pointsMerge(true)
-    // .pointRadius(0.03)
-  }, 1000)
-
-  const globeMaterial = Globe.globeMaterial()
-  globeMaterial.emissive = new Color(0x220038)
-  globeMaterial.emissiveIntensity = 0.1
-  globeMaterial.shininess = 0.7
-  globeMaterial.wireframe = true
-
-  scene.add(Globe)
-}
-
-// function onMouseMove(event) {
-//   mouseX = event.clientX - windowHalfX
-//   mouseY = event.clientY - windowHalfY
-// console.log("x: " + mouseX + " y: " + mouseY);
-// }
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  // windowHalfX = window.innerWidth / 1.5
-  // windowHalfY = window.innerHeight / 1.5
-  renderer.setSize(window.innerWidth, window.innerHeight)
-}
-
-function animate() {
-  // camera.position.x +=
-  //   Math.abs(mouseX) <= windowHalfX / 2
-  //     ? (mouseX / 2 - camera.position.x) * 0.005
-  //     : 0
-  // camera.position.y += (-mouseY / 2 - camera.position.y) * 0.005
-  camera.lookAt(scene.position)
-  controls.update()
-
-  let renderers = [renderer, cssRenderer]
-  renderers.forEach((r) => r.render(scene, camera))
-
-  requestAnimationFrame(animate)
-}
+import { init } from './constants/globe'
+import locations from 'assets/json/locations.json'
+import {
+  CircleGeometry,
+  DoubleSide,
+  Mesh,
+  MeshBasicMaterial,
+  RingGeometry,
+} from 'three'
 
 function App() {
   const [location, setLocation] = useState({ ...DEFAULT_LOCATION })
   const [nav, setNav] = useState('all')
+  const threeCanvas = useRef({
+    scene: null,
+    renderer: null,
+    cssRenderer: null,
+    globe: null,
+    camera: null,
+    controls: null,
+  })
+
+  const animate = useCallback(() => {
+    const { renderer, scene, cssRenderer, camera, controls } =
+      threeCanvas.current
+
+    camera.lookAt(scene.position)
+    controls.update()
+
+    let renderers = [renderer, cssRenderer]
+    renderers.forEach((r) => r.render(scene, camera))
+
+    requestAnimationFrame(animate)
+  }, [])
+
+  const resizeWindow = useCallback(() => {
+    const { camera, renderer } = threeCanvas.current
+
+    if (camera && renderer) {
+      camera.aspect = window.innerWidth / window.innerHeight
+      camera.updateProjectionMatrix()
+
+      renderer.setSize(window.innerWidth, window.innerHeight)
+    }
+  }, [])
 
   useEffect(() => {
-    init()
-    initGlobe()
-    onWindowResize()
+    // Init the globe
+    threeCanvas.current = { ...init() }
+    resizeWindow()
+
+    // Add the resize event
+    const controller = new AbortController()
+    window.addEventListener('resize', resizeWindow, {
+      signal: controller.signal,
+    })
+
+    // Animate
     animate()
 
     return () => {
+      // Remove the resize event
+      controller.abort()
+
       const globPlace = document.getElementById('3d-glob')
       if (globPlace) globPlace.innerHTML = ''
     }
-  }, [])
+  }, [animate, resizeWindow])
+
+  useEffect(() => {
+    if (threeCanvas.current.scene) {
+      if (nav === 'all') {
+        threeCanvas.current.Globe.customLayerData([...locations])
+        threeCanvas.current.Globe.htmlElementsData([...locations])
+      } else {
+        let filterLocations = locations.filter((loc) => loc.state === nav)
+
+        threeCanvas.current.Globe.customLayerData(filterLocations)
+          .customThreeObject((d) => {
+            let circle = new Mesh(
+              new CircleGeometry(d.isInactive ? 0.4 : 0.6)
+                .translate(0, 0, 1.01)
+                .rotateX(Math.PI),
+              new MeshBasicMaterial({ color: '#00FECD', side: DoubleSide }),
+            )
+
+            const ringInner = d.state === 'planned' ? 1.4 : 0.6
+
+            const ring = new Mesh(
+              new RingGeometry(ringInner, 1.5)
+                .translate(0, 0, 1)
+                .rotateX(Math.PI),
+              new MeshBasicMaterial({
+                color: 'rgba(4, 164, 135, 0.5)',
+                side: DoubleSide,
+              }),
+            )
+
+            circle.add(ring)
+            ring.lookAt(0, 0, 0)
+
+            return circle
+          })
+          .customThreeObjectUpdate((obj, d) => {
+            Object.assign(
+              obj.position,
+              threeCanvas.current.Globe.getCoords(d.lat, d.lng, d.alt),
+            )
+
+            obj.lookAt(0, 0, 0)
+          })
+
+        threeCanvas.current.Globe.htmlElementsData([...filterLocations])
+      }
+    }
+  }, [nav])
 
   const hdChangeLocation = (key, value) => {
     setLocation((prev) => ({ ...prev, [key]: value }))
---
+++ src/pages/home/constants/globe.js
@@ -0,0 +1,323 @@
+import {
+  AmbientLight,
+  CircleGeometry,
+  Color,
+  DirectionalLight,
+  DoubleSide,
+  Fog,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  PointLight,
+  RingGeometry,
+  Scene,
+  WebGLRenderer,
+} from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls'
+import ThreeGlobe from 'three-globe'
+
+import locations from 'assets/json/locations.json'
+import countries from 'assets/globe-data-min.json'
+import travelHistory from 'assets/my-flights.json'
+import { CSS2DRenderer } from 'three/addons/renderers/CSS2DRenderer'
+
+// var renderer, cssRenderer, camera, scene, controls
+// let mouseX = 0
+// let mouseY = 0
+// let windowHalfX = window.innerWidth / 2
+// let windowHalfY = window.innerHeight / 2
+// var Globe
+
+export const renderAirport = (name) => {
+  return `<div
+   style="color: #FAF6FE;
+   background: #ffffff29;
+   line-height: 27px;
+   font-family: Futura Round Medium;
+   padding: 0 24px;
+   border-radius: 40px;
+   font-size: 16px;
+   border: 1px solid rgba(159, 115, 202, 0.5)">
+   ${name}
+   </div>`
+}
+
+export function init() {
+  let renderer, cssRenderer, camera, scene, controls, Globe
+
+  // Initialize renderer
+  renderer = new WebGLRenderer({ antialias: true })
+  renderer.setClearColor(0x000000, 0)
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.getElementById('3d-glob').appendChild(renderer.domElement)
+
+  // Initialize renderer
+  cssRenderer = new CSS2DRenderer()
+  cssRenderer.setSize(window.innerWidth, window.innerHeight)
+  cssRenderer.domElement.style.position = 'absolute'
+  cssRenderer.domElement.style.top = '-32px'
+  cssRenderer.domElement.style.pointerEvents = 'none'
+  document.getElementById('3d-glob').appendChild(cssRenderer.domElement)
+
+  // Initialize scene, light
+  scene = new Scene()
+  // scene.background = new Color('transparent')
+  scene.add(new AmbientLight(0xbbbbbb, 0.3))
+
+  // Initialize camera, light
+  camera = new PerspectiveCamera()
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+
+  var dLight = new DirectionalLight(0xffffff, 0.8)
+  dLight.position.set(-800, 2000, 400)
+  camera.add(dLight)
+
+  var dLight1 = new DirectionalLight(0x7982f6, 1)
+  dLight1.position.set(-200, 500, 200)
+  camera.add(dLight1)
+
+  var dLight2 = new PointLight(0x8566cc, 0.5)
+  dLight2.position.set(-200, 500, 200)
+  camera.add(dLight2)
+
+  camera.position.z = 400
+  camera.position.x = 20000
+  camera.position.y = 5000
+
+  camera.zoom = 1.7
+
+  scene.add(camera)
+
+  // Additional effects
+  scene.fog = new Fog(0x535ef3, 400, 2000)
+
+  // Initialize controls
+  controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableDamping = true
+  controls.dynamicDampingFactor = 0.01
+  controls.enablePan = false
+  controls.minDistance = 200
+  controls.maxDistance = 400
+  controls.rotateSpeed = 0.8
+  controls.zoomSpeed = 1
+  controls.autoRotate = false
+
+  controls.minPolarAngle = Math.PI / 3.5
+  controls.maxPolarAngle = Math.PI - Math.PI / 3
+
+  // window.addEventListener('resize', onWindowResize, false)
+  // document.addEventListener('mousemove', onMouseMove)
+
+  // Initialize the Globe
+  Globe = new ThreeGlobe({
+    waitForGlobeReady: true,
+    animateIn: true,
+  })
+    .showGlobe(false)
+
+    .htmlElementsData(locations)
+    .htmlElement((d) => {
+      const el = document.createElement('div')
+      el.className = 'location'
+      el.innerHTML = renderAirport(d.city)
+      return el
+    })
+
+    .hexPolygonsData(countries.features)
+    .hexPolygonResolution(3)
+    .hexPolygonMargin(0.7)
+    .showAtmosphere(false)
+    // .atmosphereColor("red")
+    .atmosphereAltitude(0.25)
+    .hexPolygonColor((e) => {
+      if (
+        ['KGZ', 'KOR', 'THA', 'RUS', 'UZB', 'IDN', 'KAZ', 'MYS'].includes(
+          e.properties.ISO_A3,
+        )
+      ) {
+        return 'rgba(255,255,255, 1)'
+      } else return 'rgba(255,255,255, 0.7)'
+    })
+
+    .customLayerData(locations)
+    .customThreeObject((d) => {
+      let circle = new Mesh(
+        new CircleGeometry(d.isInactive ? 0.4 : 0.6)
+          .translate(0, 0, 1.01)
+          .rotateX(Math.PI),
+        new MeshBasicMaterial({ color: '#00FECD', side: DoubleSide }),
+      )
+
+      const ringInner = d.state === 'planned' ? 1.4 : 0.6
+
+      const ring = new Mesh(
+        new RingGeometry(ringInner, 1.5).translate(0, 0, 1).rotateX(Math.PI),
+        new MeshBasicMaterial({
+          color: 'rgba(4, 164, 135, 0.5)',
+          side: DoubleSide,
+        }),
+      )
+
+      circle.add(ring)
+      ring.lookAt(0, 0, 0)
+
+      return circle
+    })
+    .customThreeObjectUpdate((obj, d) => {
+      Object.assign(obj.position, Globe.getCoords(d.lat, d.lng, d.alt))
+
+      obj.lookAt(0, 0, 0)
+    })
+
+  // NOTE Arc animations are followed after the globe enters the scene
+  setTimeout(() => {
+    Globe.arcsData(travelHistory.flights)
+      .arcColor((e) => {
+        return e.status ? 'rgba(9, 196, 162, 1)' : 'rgba(179, 171, 184, 1)'
+      })
+      .arcAltitude((e) => {
+        return e.arcAlt
+      })
+      .arcStroke((e) => {
+        return e.status ? 0.5 : 0.3
+      })
+      .arcDashLength(0.5)
+      .arcDashGap(4)
+      .arcDashAnimateTime(1000)
+      .arcsTransitionDuration(1000)
+      .arcDashInitialGap((e) => e.order * 1)
+  }, 1000)
+
+  const globeMaterial = Globe.globeMaterial()
+  globeMaterial.emissive = new Color(0x220038)
+  globeMaterial.emissiveIntensity = 0.1
+  globeMaterial.shininess = 0.7
+  globeMaterial.wireframe = true
+
+  scene.add(Globe)
+
+  return { scene, renderer, cssRenderer, controls, camera, Globe }
+}
+
+// SECTION Globe
+// function initGlobe() {
+//   // Initialize the Globe
+//   Globe = new ThreeGlobe({
+//     waitForGlobeReady: true,
+//     animateIn: true,
+//   })
+//     .showGlobe(false)
+//
+//     .htmlElementsData(locations)
+//     .htmlElement((d) => {
+//       const el = document.createElement('div')
+//       el.innerHTML = renderAirport(d.city)
+//       return el
+//     })
+//
+//     .hexPolygonsData(countries.features)
+//     .hexPolygonResolution(3)
+//     .hexPolygonMargin(0.7)
+//     .showAtmosphere(false)
+//     // .atmosphereColor("red")
+//     .atmosphereAltitude(0.25)
+//     .hexPolygonColor((e) => {
+//       if (
+//         ['KGZ', 'KOR', 'THA', 'RUS', 'UZB', 'IDN', 'KAZ', 'MYS'].includes(
+//           e.properties.ISO_A3,
+//         )
+//       ) {
+//         return 'rgba(255,255,255, 1)'
+//       } else return 'rgba(255,255,255, 0.7)'
+//     })
+//
+//     .customLayerData(locations)
+//     .customThreeObject((d) => {
+//       let circle = new Mesh(
+//         new CircleGeometry(d.isInactive ? 0.4 : 0.6)
+//           .translate(0, 0, 1.01)
+//           .rotateX(Math.PI),
+//         new MeshBasicMaterial({ color: '#00FECD', side: DoubleSide }),
+//       )
+//
+//       const ringInner = d.state === 'planned' ? 1.4 : 0.6
+//
+//       const ring = new Mesh(
+//         new RingGeometry(ringInner, 1.5).translate(0, 0, 1).rotateX(Math.PI),
+//         new MeshBasicMaterial({
+//           color: 'rgba(4, 164, 135, 0.5)',
+//           side: DoubleSide,
+//         }),
+//       )
+//
+//       circle.add(ring)
+//       ring.lookAt(0, 0, 0)
+//
+//       return circle
+//     })
+//     .customThreeObjectUpdate((obj, d) => {
+//       Object.assign(obj.position, Globe.getCoords(d.lat, d.lng, d.alt))
+//
+//       obj.lookAt(0, 0, 0)
+//     })
+//
+//   // NOTE Arc animations are followed after the globe enters the scene
+//   setTimeout(() => {
+//     Globe.arcsData(travelHistory.flights)
+//       .arcColor((e) => {
+//         return e.status ? 'rgba(9, 196, 162, 1)' : 'rgba(179, 171, 184, 1)'
+//       })
+//       .arcAltitude((e) => {
+//         return e.arcAlt
+//       })
+//       .arcStroke((e) => {
+//         return e.status ? 0.5 : 0.3
+//       })
+//       .arcDashLength(0.5)
+//       .arcDashGap(4)
+//       .arcDashAnimateTime(1000)
+//       .arcsTransitionDuration(1000)
+//       .arcDashInitialGap((e) => e.order * 1)
+//   }, 1000)
+//
+//   const globeMaterial = Globe.globeMaterial()
+//   globeMaterial.emissive = new Color(0x220038)
+//   globeMaterial.emissiveIntensity = 0.1
+//   globeMaterial.shininess = 0.7
+//   globeMaterial.wireframe = true
+//
+//   return Globe
+// }
+
+// function onMouseMove(event) {
+//   mouseX = event.clientX - windowHalfX
+//   mouseY = event.clientY - windowHalfY
+// console.log("x: " + mouseX + " y: " + mouseY);
+// }
+
+// function onWindowResize() {
+//   camera.aspect = window.innerWidth / window.innerHeight
+//   camera.updateProjectionMatrix()
+//   // windowHalfX = window.innerWidth / 1.5
+//   // windowHalfY = window.innerHeight / 1.5
+//   renderer.setSize(window.innerWidth, window.innerHeight)
+// }
+
+// function animate() {
+//   // camera.position.x +=
+//   //   Math.abs(mouseX) <= windowHalfX / 2
+//   //     ? (mouseX / 2 - camera.position.x) * 0.005
+//   //     : 0
+//   // camera.position.y += (-mouseY / 2 - camera.position.y) * 0.005
+//   camera.lookAt(scene.position)
+//   controls.update()
+//
+//   let renderers = [renderer, cssRenderer]
+//   renderers.forEach((r) => r.render(scene, camera))
+//
+//   requestAnimationFrame(animate)
+// }
+
+// export { init, onWindowResize }
